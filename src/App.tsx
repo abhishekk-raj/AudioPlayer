1fd754ca9d57b441039c24d837540744433b676c
Abhishek Raj <abhishek@betatestsolutions.com>
2022-12-22 11:22:44 +0530

    Added Christmas banner.

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import {musicTracks} from "./assets/music-tracks";
 function App() {
     return (
         <div className="App">
+            <h1 className='merryChristmas'>Merry Christmas</h1>
             <MusicPlayer tracks={musicTracks}/>
         </div>
     );
